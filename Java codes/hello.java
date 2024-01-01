043c300f4fceaf3fc4819dd88cc430566530361b
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2024-01-01 21:16:42 +0530

    Add files via upload

---
+++ Java codes/hello.java
@@ -0,0 +1,13 @@
+public class hello{
+   public static void main(String[] args) {
+        System.out.print("hello");
+        System.out.println(args[0]);
+        //println for requesting new line
+
+        System.out.println("heyy");
+        System.out.println(args[1]);
+
+        int a=10;
+        System.out.println(a);
+    }
+}
