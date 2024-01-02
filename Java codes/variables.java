0fed82d6734d8415f97924d14631d01b4466625b
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2024-01-02 19:50:49 +0530

    Add files via upload

---
+++ Java codes/variables.java
@@ -0,0 +1,34 @@
+public class variables {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a); //sout
+        a=20;
+        System.out.println(a);
+        //10 20
+
+        //variables
+        //identifiers(consists of all var,class etc.)
+
+        //non primitive (user defined)
+        //primitive (built in)
+        //(double float) char boolean (byte int short long)
+
+        boolean flag = true; //true or false
+        int i=4;
+        short s=2;
+        long l=8;
+        byte b=1;
+        float f=4.0f;
+        double d=8.0;
+        char c='2';
+        System.out.println("boolean "+flag);
+        System.out.println("byte "+b);
+        System.out.println("short "+s);
+        System.out.println("int "+i);
+        System.out.println("long "+l);
+        System.out.println("float "+f);
+        System.out.println("double "+d);
+        System.out.println("char "+c);
+
+    }
+}
