6ddffbf09ae1bbdb19e352e412fcee89d71bcc3b
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2024-01-06 23:48:04 +0530

    oops.java

--- Java codes/oops.java
+++ Java codes/oops.java
@@ -13,9 +13,17 @@ public class oops {
         //creating obj
         //className objName = new className();
         employee a=new employee();
-        System.out.println(a.age);
+        System.out.println(a.age); //30
         System.out.println(a.salary); //int - 0
         System.out.println(a.name); //string - null
+
+        employee b=new employee();
+        b.salary=100000;
+        b.name="Ariana";
+        b.age=28;
+        System.out.println(b.age); //28
+        System.out.println(b.salary); //100000
+        System.out.println(b.name); //Ariana
         
     }
 }
