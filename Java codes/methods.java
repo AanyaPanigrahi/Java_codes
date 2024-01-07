dff8f9d5cb7ece3e9cb63805b2f4d1a197a75b4d
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2024-01-07 23:40:02 +0530

    Add files via upload

---
+++ Java codes/methods.java
@@ -0,0 +1,9 @@
+public class methods {
+
+    static void welcome(){
+        System.out.println("welcome");
+    }
+    public static void main(String[] args) {
+        welcome(); //call a function
+    }
+}
